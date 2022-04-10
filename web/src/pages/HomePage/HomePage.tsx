4fec4c759967416e727b91b5d040eeff76b19528
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-04-10 20:32:49 +0100

    add hero to landing page + tw forms

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Link } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
@@ -6,14 +6,83 @@ const HomePage = () => {
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
+        <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+          <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+            <h1>
+              <span className="block text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
+                Coming soon
+              </span>
+              <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl">
+                <span className="block text-gray-900">Next generation</span>
+                <span className="block text-indigo-600">IVF Management</span>
+              </span>
+            </h1>
+            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+              A modern cloud-based web application for tracking your patients
+              IVF Cycle from Day 1.
+            </p>
+            <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
+              <p className="text-base font-medium text-gray-900">
+                Sign up to get notified when it’s ready.
+              </p>
+              <form action="#" method="POST" className="mt-3 sm:flex">
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="block w-full py-3 text-base rounded-md placeholder-gray-500 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:flex-1 border-gray-300"
+                  placeholder="Enter your email"
+                />
+                <button
+                  type="submit"
+                  className="mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
+                >
+                  Notify me
+                </button>
+              </form>
+              <p className="mt-3 text-sm text-gray-500">
+                We care about the protection of your data. Read our{' '}
+                <Link to="#" className="font-medium text-gray-900 underline">
+                  Privacy Policy
+                </Link>
+                .
+              </p>
+            </div>
+          </div>
+          <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+            <div className="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+              <button
+                type="button"
+                className="relative block w-full bg-white rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="sr-only">Watch our video to learn more</span>
+                <img
+                  className="w-full"
+                  src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                  alt=""
+                />
+                <div
+                  className="absolute inset-0 w-full h-full flex items-center justify-center"
+                  aria-hidden="true"
+                >
+                  <svg
+                    className="h-20 w-20 text-indigo-500"
+                    fill="currentColor"
+                    viewBox="0 0 84 84"
+                  >
+                    <circle opacity="0.9" cx={42} cy={42} r={42} fill="white" />
+                    <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
+                  </svg>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
