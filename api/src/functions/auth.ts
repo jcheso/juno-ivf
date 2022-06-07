aaf376a29a121a4f7a52daba547be7c31a0bdb76
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-06-07 10:52:28 +0100

    trim whitespace from email on registration

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -115,9 +115,11 @@ export const handler = async (event, context) => {
     // If this returns anything else, it will be returned by the
     // `signUp()` function in the form of: `{ message: 'String here' }`.
     handler: ({ username, hashedPassword, salt, userAttributes }) => {
+      // Trim any whitespace before/after username entered
+      const trimmedEmail = username.trim()
       return db.user.create({
         data: {
-          email: username,
+          email: trimmedEmail,
           hashedPassword: hashedPassword,
           salt: salt,
           firstName: userAttributes.firstName,
