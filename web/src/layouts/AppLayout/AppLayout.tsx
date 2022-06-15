e9fcc8e43e8f1b8a5f812decfeebc101b48ecd72
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-06-16 06:38:41 +1000

    Show logged in user email in status bar and add drop down

--- web/src/layouts/AppLayout/AppLayout.tsx
+++ web/src/layouts/AppLayout/AppLayout.tsx
@@ -12,7 +12,9 @@ import {
   BeakerIcon,
   ClipboardListIcon,
   PhotographIcon,
+  ChevronDownIcon,
 } from '@heroicons/react/outline'
+import { UserIcon } from '@heroicons/react/solid'
 
 import { useAuth } from '@redwoodjs/auth'
 import { Link, navigate, NavLink, routes } from '@redwoodjs/router'
@@ -32,7 +34,8 @@ type AppLayoutProps = {
 
 const AppLayout = ({ children }: AppLayoutProps) => {
   const [sidebarOpen, setSidebarOpen] = useState(false)
-  const { logOut } = useAuth()
+  const { logOut, currentUser } = useAuth()
+  console.log(currentUser)
   const [patient, setPatient] = React.useContext(PatientContext)
   const [activeTreatment, setActiveTreatment] = useContext(TreatmentContext)
   const patientHidden = patient == null ? true : false
@@ -238,40 +241,22 @@ const AppLayout = ({ children }: AppLayoutProps) => {
               <span className="sr-only">Open sidebar</span>
               <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
             </button>
-            <div className="flex-1 px-4 flex justify-between flex-row">
-              <StatusBar
-                patient={patient}
-                activeTreatment={activeTreatment}
-              ></StatusBar>
+            <div className="flex-1 px-4 flex justify-between sm:px-6 lg:mx-auto lg:px-8">
+              <div className="flex-1 flex">
+                <StatusBar></StatusBar>
+              </div>
               <div className="ml-4 flex items-center md:ml-6">
-                {patient !== null && (
-                  <button
-                    type="button"
-                    onClick={() => {
-                      navigate(routes.dashboard())
-                      setTimeout(() => {
-                        setPatient(null)
-                        setActiveTreatment(null)
-                        localStorage.setItem('patient', '')
-                        localStorage.setItem('activeTreatment', '')
-                      }, 50)
-                    }}
-                    className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    <span className="sr-only">Clear patient</span>
-                    <XIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                )}
-
-                {/* Profile dropdown */}
                 <Menu as="div" className="ml-3 relative">
                   <div>
-                    <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                    <Menu.Button className="max-w-xs bg-white rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 lg:p-2 lg:rounded-md lg:hover:bg-gray-50">
+                      <UserIcon className="h-8 w-8 rounded-full" />
+                      <span className="hidden ml-3 text-gray-700 text-sm font-medium lg:block">
+                        <span className="sr-only">Open user menu for </span>
+                        {currentUser.email}
+                      </span>
+                      <ChevronDownIcon
+                        className="hidden flex-shrink-0 ml-1 h-5 w-5 text-gray-400 lg:block"
+                        aria-hidden="true"
                       />
                     </Menu.Button>
                   </div>
