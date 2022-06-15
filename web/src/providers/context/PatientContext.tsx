bd3c968974dbe70d5f46a39b6e1375c4a18e3221
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-06-15 15:59:52 +1000

    check for server or client before using localstorage

--- web/src/providers/context/PatientContext.tsx
+++ web/src/providers/context/PatientContext.tsx
@@ -5,15 +5,17 @@ const PatientContext = createContext(null)
 const PatientContextProvider = ({ children }) => {
   const [state, setState] = React.useState(null)
 
-  const patientCache = JSON.parse(localStorage.getItem('patientCache'))
-  // if the state is not set
+  if (typeof window !== 'undefined') {
+    const patientCache = JSON.parse(localStorage.getItem('patientCache'))
+    // if the state is not set
 
-  if (
-    state == null &&
-    patientCache &&
-    new Date(patientCache.expires) > new Date()
-  ) {
-    setState(patientCache.value)
+    if (
+      state == null &&
+      patientCache &&
+      new Date(patientCache.expires) > new Date()
+    ) {
+      setState(patientCache.value)
+    }
   }
 
   return (
