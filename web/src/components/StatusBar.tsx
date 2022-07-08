01405a6f0698dea6e32e0430e02b0e95ff4058b9
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-07-08 08:05:54 +0100

    Remove dropdown menu if no context

--- web/src/components/StatusBar.tsx
+++ web/src/components/StatusBar.tsx
@@ -54,63 +54,65 @@ const StatusBar = () => {
               ? `Cycle ${activeTreatment.count}`
               : 'No cycle selected'}
           </span>
-          <Menu as="div" className="ml-3 relative inline-block text-left">
-            <div>
-              <Menu.Button className="-my-2 p-2 rounded-full bg-white flex items-center text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500">
-                <span className="sr-only">Open options</span>
-                <DotsVerticalIcon className="h-5 w-5" aria-hidden="true" />
-              </Menu.Button>
-            </div>
+          {patient !== null && (
+            <Menu as="div" className="ml-3 relative inline-block text-left">
+              <div>
+                <Menu.Button className="-my-2 p-2 rounded-full bg-white flex items-center text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                  <span className="sr-only">Open options</span>
+                  <DotsVerticalIcon className="h-5 w-5" aria-hidden="true" />
+                </Menu.Button>
+              </div>
 
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="py-1">
-                  {activeTreatment !== null && (
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          onClick={() => navigate(routes.treatments())}
-                          className={classNames(
-                            active
-                              ? 'bg-gray-100 text-gray-900'
-                              : 'text-gray-700',
-                            'w-full px-4 py-2 h-full text-left text-sm'
-                          )}
-                        >
-                          <span>Update Treatment</span>
-                        </button>
-                      )}
-                    </Menu.Item>
-                  )}
-                  {patient !== null && (
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          onClick={() => clearContext()}
-                          className={classNames(
-                            active
-                              ? 'bg-gray-100 text-gray-900'
-                              : 'text-gray-700',
-                            'w-full px-4 py-2 h-full text-left text-sm'
-                          )}
-                        >
-                          <span>Deselect Patient</span>
-                        </button>
-                      )}
-                    </Menu.Item>
-                  )}
-                </div>
-              </Menu.Items>
-            </Transition>
-          </Menu>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="py-1">
+                    {activeTreatment !== null && (
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={() => navigate(routes.treatments())}
+                            className={classNames(
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'w-full px-4 py-2 h-full text-left text-sm'
+                            )}
+                          >
+                            <span>Update Treatment</span>
+                          </button>
+                        )}
+                      </Menu.Item>
+                    )}
+                    {patient !== null && (
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={() => clearContext()}
+                            className={classNames(
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'w-full px-4 py-2 h-full text-left text-sm'
+                            )}
+                          >
+                            <span>Deselect Patient</span>
+                          </button>
+                        )}
+                      </Menu.Item>
+                    )}
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          )}
         </div>
       </div>
     </div>
