c9dee6adfe3d13c0d0731b8c348d9e9c8996080c
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-07-12 14:27:14 +0100

    Fix broken reference

--- web/src/pages/UltrasoundsPage/UltrasoundsPage.tsx
+++ web/src/pages/UltrasoundsPage/UltrasoundsPage.tsx
@@ -1,13 +1,10 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
-import CalendarView from 'src/components/Ultrasounds/CalendarView'
 const UltrasoundsPage = () => {
   return (
     <>
       <MetaTags title="Ultrasounds" description="Ultrasounds page" />
-
-      <CalendarView />
     </>
   )
 }
