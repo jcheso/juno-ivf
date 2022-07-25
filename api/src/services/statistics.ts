bcae4103dfada861b8017cac8b0f1e1c35057be0
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-07-25 19:12:34 +0100

    Avoid dividing by zero and fix changeTypes

--- api/src/services/statistics.ts
+++ api/src/services/statistics.ts
@@ -24,65 +24,73 @@ export const statistics = async () => {
     stat: (await previousPatientCount) + (await patientsAddedInLast30Days),
     previousStat: await previousPatientCount,
     change: patientCountChangeRate.toFixed(0),
-    changeType: patientCountChangeRate > 0 ? 'increase' : 'decrease',
+    changeType:
+      patientCountChangeRate > 0
+        ? 'increase'
+        : patientCountChangeRate < 0
+        ? 'decrease'
+        : 'no change',
   }
 
   // Treatments added in last 30 days
-  const treatmentsAddedInLast30Days = db.treatment.count({
+  const treatmentsAddedInLast30Days = await db.treatment.count({
     where: { createdAt: { lte: endDate, gte: startDate } },
   })
 
-  const previousTreatmentCount = db.treatment.count({
+  const previousTreatmentCount = await db.treatment.count({
     where: { createdAt: { lte: startDate } },
   })
+  let treatmentCountChangeRate = 0
 
-  let treatmentCountChangeRate =
-    ((await treatmentsAddedInLast30Days) / (await previousTreatmentCount)) * 100
-
-  if (treatmentCountChangeRate === Infinity) {
-    treatmentCountChangeRate = 0.0
+  if (previousTreatmentCount !== 0) {
+    treatmentCountChangeRate =
+      (treatmentsAddedInLast30Days / previousTreatmentCount) * 100
   }
 
   const totalTreatmentsCount =
-    (await previousTreatmentCount) + (await treatmentsAddedInLast30Days)
+    previousTreatmentCount + treatmentsAddedInLast30Days
 
   const totalTreatments = {
     name: 'Total Treatments',
     stat: totalTreatmentsCount,
     previousStat: await previousTreatmentCount,
     change: treatmentCountChangeRate.toFixed(0),
-    changeType: treatmentCountChangeRate > 0 ? 'increase' : 'decrease',
+    changeType:
+      treatmentCountChangeRate > 0
+        ? 'increase'
+        : treatmentCountChangeRate < 0
+        ? 'decrease'
+        : 'no change',
   }
 
   // Successful Treatments Rate without last 30 days
-  const prevSuccessfulTreatments = db.treatment.count({
+  const prevSuccessfulTreatments = await db.treatment.count({
     where: {
       createdAt: { lte: startDate },
       wasSuccessful: true,
     },
   })
 
-  const successfulTreatments = db.treatment.count({
+  const successfulTreatments = await db.treatment.count({
     where: {
       wasSuccessful: true,
     },
   })
 
-  let prevSuccessfulTreatmentsRate =
-    ((await prevSuccessfulTreatments) / (await previousTreatmentCount)) * 100
-  let successfulTreatmentsRate =
-    ((await successfulTreatments) / totalTreatmentsCount) * 100
-
-  if (isNaN(prevSuccessfulTreatmentsRate)) {
-    prevSuccessfulTreatmentsRate = 0.0
+  let prevSuccessfulTreatmentsRate = 0.0
+  if (previousTreatmentCount !== 0) {
+    prevSuccessfulTreatmentsRate =
+      (prevSuccessfulTreatments / previousTreatmentCount) * 100
   }
-  if (isNaN(successfulTreatmentsRate)) {
-    successfulTreatmentsRate = 0.0
+
+  let successfulTreatmentsRate = 0.0
+  if (totalTreatmentsCount !== 0) {
+    successfulTreatmentsRate =
+      (successfulTreatments / totalTreatmentsCount) * 100
   }
 
   const successfulTreatmentRateChangeRate =
-    (await successfulTreatmentsRate) - (await prevSuccessfulTreatmentsRate)
-
+    successfulTreatmentsRate - prevSuccessfulTreatmentsRate
   const successfulTreatmentsRateStat = {
     name: 'Successful Treatments Rate',
     stat: `${successfulTreatmentsRate.toFixed(0)}%`,
